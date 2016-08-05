725c6a44b200556129b7c4d0045718aeadd9114d
mczech <mczech@thoughtworks.com>
2016-08-05 10:35:40 +0200

    refactoring

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,11 +2,13 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
+    public static final String WELCOME_MESSAGE = "Welcome to Biblioteca!";
+
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
 
     public void showWelcomeMessage() {
-        System.out.println("Welcome to Biblioteca!");
+        System.out.println(WELCOME_MESSAGE);
     }
 }
