8f1a20aade0d400c99761c24e42077cf909b7da1
mczech <mczech@thoughtworks.com>
2016-08-05 13:41:20 +0200

    made test pass

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -4,11 +4,13 @@ package com.twu.biblioteca;
  * Created by mczech on 05/08/16.
  */
 public class Menu {
+
     public void addOption(String s) {
 
     }
 
     public void show() {
-
+        System.out.println("a) List Options");
     }
+
 }
