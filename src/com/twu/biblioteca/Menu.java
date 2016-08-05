75536bf03f5ad78a9018fc7965398b4fd9f45d70
mczech <mczech@thoughtworks.com>
2016-08-05 13:39:03 +0200

    tested if showing a menu produces expected output

---
+++ src/com/twu/biblioteca/Menu.java
@@ -0,0 +1,14 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by mczech on 05/08/16.
+ */
+public class Menu {
+    public void addOption(String s) {
+
+    }
+
+    public void show() {
+
+    }
+}
---
+++ test/com/twu/biblioteca/MenuTest.java
@@ -0,0 +1,31 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mczech on 05/08/16.
+ */
+public class MenuTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void showMenu() {
+        Menu menu = new Menu();
+        menu.addOption("List Options");
+        menu.show();
+        assertEquals("a) List Options", outContent.toString());
+    }
+
+}
