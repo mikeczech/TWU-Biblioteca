35ec91fef1714a6239c65efdc84e5aa92a5230df
mczech <mczech@thoughtworks.com>
2016-08-06 18:23:22 +0200

    made test pass

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,15 +5,29 @@ public class BibliotecaApp {
 
     public static final String WELCOME_MESSAGE = "Welcome to Biblioteca!";
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    private Menu menu = new Menu();
+
+    public BibliotecaApp() {
+        buildMenu();
     }
 
     public void showWelcomeMessage() {
         System.out.println(WELCOME_MESSAGE);
     }
 
-    public void startAndThenQuit() {
-
+    private void buildMenu() {
+        menu.addOption("List Books");
+        menu.addOption("Quit");
     }
+
+    public void startAndThenQuit() {
+        showWelcomeMessage();
+        System.out.println();
+        menu.show();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
 }
