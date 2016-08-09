df879c041415d4fbab1599c3b4b3a501c5fcb263
mczech <mczech@thoughtworks.com>
2016-08-09 17:52:31 +0200

    Eliminated another warning from IntelliJ

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -15,12 +15,12 @@ class BibliotecaApp {
     }
 
     public static void main(String[] args) {
-        Library bookLibrary = new Library(
+        Library bookLibrary = new Library<>(
                 new Book("Brave New World", "Aldous Huxley", Year.of(1932)),
                 new Book("Animal Farm", "George Orwell", Year.of(1945)),
                 new Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", Year.of(1979))
         );
-        Library movieLibrary = new Library(
+        Library movieLibrary = new Library<>(
                 new Movie("E.T. the Extra-Terrestrial", "Steven Spielberg", Year.of(1982), Rating.UNRATED),
                 new Movie("The Hobbit", "Peter Jackson", Year.of(2012), Rating.of(3)),
                 new Movie("The Terminator", "James Cameron", Year.of(1984), Rating.of(9))
