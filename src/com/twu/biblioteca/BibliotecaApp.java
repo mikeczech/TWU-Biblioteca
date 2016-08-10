613efdf8b061c3b4407678ce0c4e04d8a4595dd9
mczech <mczech@thoughtworks.com>
2016-08-10 17:24:32 +0200

    Refactoring of BibliotecaApp class

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -6,8 +6,33 @@ class BibliotecaApp {
 
     private final ConsoleUI ui;
 
-    private BibliotecaApp(SecureLibrary bookLibrary, Library movieLibrary) {
-        ui = new ConsoleUI(bookLibrary, movieLibrary);
+    private SecureLibrary books;
+    private Library movies;
+
+    private BibliotecaApp() {
+        fillLibraries();
+        ui = new ConsoleUI(books, movies);
+    }
+
+    private void fillLibraries() {
+        fillBookLibrary();
+        fillMovieLibrary();
+    }
+
+    private void fillBookLibrary() {
+        books = new SecureLibrary<>(
+                new Book("Brave New World", "Aldous Huxley", Year.of(1932)),
+                new Book("Animal Farm", "George Orwell", Year.of(1945)),
+                new Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", Year.of(1979))
+        );
+    }
+
+    private void fillMovieLibrary() {
+        movies = new Library<>(
+                new Movie("E.T. the Extra-Terrestrial", "Steven Spielberg", Year.of(1982), Rating.UNRATED),
+                new Movie("The Hobbit", "Peter Jackson", Year.of(2012), Rating.of(3)),
+                new Movie("The Terminator", "James Cameron", Year.of(1984), Rating.of(9))
+        );
     }
 
     private void start() {
@@ -15,17 +40,7 @@ class BibliotecaApp {
     }
 
     public static void main(String[] args) {
-        SecureLibrary bookLibrary = new SecureLibrary<>(
-                new Book("Brave New World", "Aldous Huxley", Year.of(1932)),
-                new Book("Animal Farm", "George Orwell", Year.of(1945)),
-                new Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", Year.of(1979))
-        );
-        Library movieLibrary = new Library<>(
-                new Movie("E.T. the Extra-Terrestrial", "Steven Spielberg", Year.of(1982), Rating.UNRATED),
-                new Movie("The Hobbit", "Peter Jackson", Year.of(2012), Rating.of(3)),
-                new Movie("The Terminator", "James Cameron", Year.of(1984), Rating.of(9))
-        );
-        BibliotecaApp app = new BibliotecaApp(bookLibrary, movieLibrary);
+        BibliotecaApp app = new BibliotecaApp();
         app.start();
     }
 
